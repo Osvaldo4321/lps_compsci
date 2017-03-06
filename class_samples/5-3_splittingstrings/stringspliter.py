04dfc92fd0d896bedbda4d49f8766444b9d724e8
Osvaldo Munoz <rc.munoz.osvaldo@leadps.org>
2017-03-06 18:38:02 +0000

    FINALLY

---
+++ class_samples/5-3_splittingstrings/stringspliter.py
@@ -0,0 +1,16 @@
+# stringspliter.py
+
+name = "Josephine Jones Hannah Paula James"
+nameSplit = name.split(" ")
+
+print(nameSplit)
+
+
+raceTimes = "59,55, 57, 54"
+
+raceTimesSplit = raceTimes.split(".")
+
+print(raceTimesSplit)
+
+
+
